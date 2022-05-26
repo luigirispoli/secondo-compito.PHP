343328c43a54bc69bb637126dcc9b70fc816f234
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-05-26 09:52:21 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,16 +2,42 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+
+$db_name = "LWndb";
+$LWuser_table_name = "LWuser";
+$LWticket_table_name = "LWticket";
+$LWabbonamenti_table_name = "LWabbonamenti";
+$LWstreaming_table_name = "LWstreaming";
+
+// connessione al db
+$mysqliConnection = new mysqli("localhost", "lweb36", "lweb36", $db_name);
+
 if (isset($_POST['invio']))         
-	
   if (empty($_POST['username']) || empty($_POST['password']))
     echo "<p> <h3>Inserire i dati per accedere !!</h3> </p>";
-  else                               
-    if ( ($_POST['username']=="utente1") && ($_POST['password']=="circo") ){
-      session_start();
-      $_SESSION['username']=$_POST['username']; 
-      exit();
-    }
+  else{       
+        $sql = "SELECT *
+               FROM $LWuser_table_name
+               WHERE username = \"{$_POST['username']}\" AND password =\"{$_POST['password']}\" ";
+	
+		if (!$result = mysqli_query($mysqliConnection, $sql)) {
+        printf("Nessun risultato...\n");
+        exit();
+        }
+	 
+	 $utente = mysqli_fetch_array($result);  // utente1 per adesso solo un utente cioe utente1
+     if ($utente) {  
+       session_start();
+       $_SESSION['username']=$_POST['username'];
+       $_SESSION['dataLogin']=time();  // per adesso dopo da togliere???
+       $_SESSION['idUtente']=$utente['userId'];
+       $_SESSION['accessoPermesso']=1000;
+       header('Location: inizio.php');    // pagina iniziale
+       exit();
+      }
+      else
+      echo "<p>Accesso negato!!!</p>";
+  } 
 ?>
 
 <?php
