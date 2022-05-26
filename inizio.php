918705962b569a70aa9a9699908b1883b6b8650c
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-05-26 09:42:19 +0200

    Update inizio.php

--- inizio.php
+++ inizio.php
@@ -7,43 +7,44 @@ if (!isset($_SESSION['accessoPermesso']))
   header('Location: login.php');
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
+
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html
 PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>Sito per comprare qualcosa che dobbiamo ancora decidere</title>
+<title>Acquisto biglietti abbonamenti e partite in streaming</title>
 <?php echo $stile; ?>
 </head>
 <body>
-<hr>
+<hr />
 <?php
 require("menu.php");
 ?>
-<hr>
-<h3>
-Benvenuto <?php echo $_SESSION['userName'];?>.
-</h3>
+<h3> Benvenuto <?php echo $_SESSION['username'];?>. </h3>
 <p>
-Ti sei collegato alle
-<?php echo date('g:i a', $_SESSION['dataLogin']);?>.
+ Il tuo accesso è stato effettuato alle 
+ <?php echo date('g:i a', $_SESSION['dataLogin']);?>.
 </p>
 <p>
-Negozio online di non so cosa ancora.
+Qui potrai acquistare biglietti e abbonamenti per seguire la tua squadra del cuore...
 </p>
-<hr>
+<hr />
+
 <?php
-echo "\$_COOKIE: ";
-print_r ($_COOKIE);
-echo "<br />";
-echo "\$_POST: ";
-print_r ($_POST);
-echo "<br />";
-echo "\$_GET: ";
-print_r ($_GET);
-echo "<br />";
-echo "\$_SESSION: ";
-print_r ($_SESSION);
-?>
+//echo "\$_COOKIE: ";
+//print_r ($_COOKIE);
+//echo "<br />";
+//echo "\$_POST: ";
+//print_r ($_POST);
+//echo "<br />";
+//echo "\$_GET: ";
+//print_r ($_GET);
+//echo "<br />";
+//echo "\$_SESSION: ";
+//print_r ($_SESSION);
+?> 
+
 </body>
 </html>
