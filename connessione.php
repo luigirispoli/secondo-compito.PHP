3dd52a4b56d229a1946c5fe484a909a080245944
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-05-26 09:40:27 +0200

    Update connessione.php
    Connessione al db usato.

--- connessione.php
+++ connessione.php
@@ -1,9 +1,11 @@
 <?php
-$db_name = "db";
-$user_table_name = "STuser";
-$p1_table_name = "p1";
-$p2_table_name = "p2";
-$mysqliConnection = new mysqli("localhost", "lweb27", "lweb27", $db_name);
+$db_name = "LWndb";
+$LWuser_table_name = "LWuser";
+$LWticket_table_name = "LWticket";
+$LWabbonamenti_table_name = "LWabbonamenti";
+$LWstreaming_table_name = "LWstreaming";
+
+$mysqliConnection = new mysqli("localhost", "lweb36", "lweb36", $db_name);
 if (mysqli_connect_errno($mysqliConnection)) {
     printf("Abbiamo problemi con la connessione al db: %s\n", mysqli_connect_error($mysqliConnection));
     exit();
