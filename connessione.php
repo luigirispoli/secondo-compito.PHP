7551fa2423736ff6a1391a8c6de5531c87f21f2d
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-06-01 00:24:17 +0200

    Update connessione.php

--- connessione.php
+++ connessione.php
@@ -1,13 +1,10 @@
 <?php
-$db_name = "LWndb";
+$db_name = "LWtdb";
 $LWuser_table_name = "LWuser";
 $LWticket_table_name = "LWticket";
 $LWabbonamenti_table_name = "LWabbonamenti";
 $LWstreaming_table_name = "LWstreaming";
 
 $mysqliConnection = new mysqli("localhost", "lweb36", "lweb36", $db_name);
-if (mysqli_connect_errno($mysqliConnection)) {
-    printf("Abbiamo problemi con la connessione al db: %s\n", mysqli_connect_error($mysqliConnection));
-    exit();
-}
+
 ?>
