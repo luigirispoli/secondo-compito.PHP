96ed86d90f2d492c6de3b7eddd7666270217d504
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-06-01 00:28:25 +0200

    Update paga.php

--- paga.php
+++ paga.php
@@ -5,32 +5,24 @@ session_start();
 if (!isset($_SESSION['accessoPermesso'])) header('Location: login.php');
 //if (!$_SESSION['carrello']) session_register("carrello");
 
-/* *********
-dati sui nomi delle tabelle e del database, nonche' sulle
-modalita' di connessione e di selezione del database: sono messi in un file a parte
-*/
 require_once("./connessione.php");
 require_once("./stile.php");
-/* ********* */
 
 
-
-// costruzione parte dell'output con l'elenco e il costo degli articoli
-//
 $_SESSION['sommeDaPagare']=0;
-$outputTable="<h3>Gentile cliente ";
-$outputTable.= $_SESSION['userName'];
+$output="<h3>Gentile ";
+$output.= $_SESSION['username'];
 
 if (!$_SESSION['carrello']) {
-   $outputTable.= " che ci fai qui con un carrello vuoto?\n";
+   $output.= " il tuo carrello è vuoto riempilo e torna qui... \n";
 } else {
-    $outputTable.=", stai per acquistare i seguenti articoli:</h3>\n";
-    $outputTable.="<table>\n";
+    $output.=", stai per acquistare i seguenti articoli:</h3>\n";
+    $output.="<table>\n";
 
     foreach ($_SESSION['carrello'] as $k=>$v) {
-      $outputTable.="<tr>\n<td>$v</td>\n";
+      $output.="<tr>\n<td>$v</td>\n";
 
-       // adesso serve il prezzo ...
+      //prezzo articoli
        $sql1 = "SELECT *
               FROM $LWticket_table_name
               WHERE partita = \"$v\"
@@ -44,31 +36,32 @@ if (!$_SESSION['carrello']) {
               WHERE partita = \"$v\"
        ";
 
-       if (!$resultQ = mysqli_query($mysqliConnection, $sql1)) {
-             printf("Dammit! Can't execute ticket select query.\n");
+       if (!$result = mysqli_query($mysqliConnection, $sql1)) {
+             printf("Can't execute ticket select query.\n");
              exit();
        }
-       $row1= mysqli_fetch_array($resultQ); 
-       if (!$resultQ = mysqli_query($mysqliConnection, $sql2)) {
-             printf("Dammit! Can't execute abbonamenti select query.\n");
+       $row1= mysqli_fetch_array($result); 
+	   
+       if (!$result = mysqli_query($mysqliConnection, $sql2)) {
+             printf("Can't execute abbonamenti select query.\n");
              exit();
        }
-       $row2= mysqli_fetch_array($resultQ); 
+       $row2= mysqli_fetch_array($result); 
+	   
+       if (!$result = mysqli_query($mysqliConnection, $sql3)) {
+             printf("Can't execute streaming select query.\n");
+             exit();
+       }
+       $row3= mysqli_fetch_array($result);
        
-       if (!$resultQ = mysqli_query($mysqliConnection, $sql3)) {
-             printf("Dammit! Can't execute streaming select query.\n");
-             exit();
-       }
-       $row3= mysqli_fetch_array($resultQ);
-       
-      $outputTable.="<td>(&euro; ";
-      $outputTable.= $row3['costoPartita']+$row2['costoAbbonamento']+$row1['costoTicket'];
-      $outputTable.=")</td>\n</tr>\n";
+      $output.="<td>(&euro; ";
+      $output.= $row3['costoPartita'] + $row2['costoAbbonamento'] + $row1['costoTicket'];
+      $output.=")</td>\n</tr>\n";
 
       $_SESSION['sommeDaPagare']+=$row3['costoPartita']+$row2['costoAbbonamento']+$row1['costoTicket'];
     }
 
-    $outputTable.="</table>\n<p>costo: {$_SESSION['sommeDaPagare']} &euro;</p>\n\n";
+    $output.="</table>\n<p>costo: {$_SESSION['sommeDaPagare']} &euro;</p>\n\n";
   }
             // fine outputTable
 ?>
@@ -87,16 +80,16 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 <hr />
 <?php
 require("menu.php");
+echo $stile;
 ?>
 <hr />
-<h2>regolamento dei conti</h2>
+<h2>Vuoi pagare??</h2>
 <?php
-echo $outputTable;
+echo $output;
 ?>
 
-<form action="zona.pagamenti.php"  method="post" >
+<form action="pagamenti.php"  method="post" >
 <p><input type="submit" name="invioPagamento" value="Procedi con il pagamento"/></p>
-<p><input type="submit" name="invioPagamento" value="Scappa di corsa"/></p>
 </form>
 </body>
 </html>
