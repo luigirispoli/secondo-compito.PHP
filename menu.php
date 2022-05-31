a206a44484bb211f6bd5f6bdd0a35f5b2f11bc80
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-06-01 00:23:45 +0200

    Inserimento link
    Ad ogni voce del menu corrispondono adesso tutti i link dei file .php

--- menu.php
+++ menu.php
@@ -1,26 +1,26 @@
 <table class="tab" border="1">
 <tr> <td style="text-align: left;">
-     <a href="mysql.biglietti.php" alt="aa">Aggiungi nel carrello i biglietti che vuoi comprare</a>
+     <a href="mysql.biglietti.php" alt="aa">Scegli i biglietti che vuoi acquistare</a>
      </td>
 </tr>
 <tr> <td style="text-align: left;">
-     <a href="link per abbonamenti" alt="aa">Acquista un abbonamento</a>
+     <a href="mysql.abbonamenti.php" alt="aa">Segli un abbonamento</a>
      </td> 
 </tr>
 <tr> <td style="text-align: left;">
-     <a href="link per partite in streaming" alt="aa">Acquista una partita in streaming</a>
+     <a href="mysql.pstreaming.php" alt="aa">Scegli una partita in streaming</a>
      </td> 
 </tr>
 <tr> <td style="text-align: left;">
-     <a href="link per eliminare cose dal carrello" alt="aa">elimina oggetti dal carrello</a>
+     <a href="elimina.php" alt="aa">elimina oggetti dal carrello</a>
      </td>
 </tr>
 <tr> <td style="text-align: left;">
-     <a href="limk per pagare cose nel carrello" alt="aa">Paga i tuoi acquisti</a>
+     <a href="paga.php" alt="aa">Paga i tuoi acquisti</a>
      </td>
 </tr>
 <tr> <td style="text-align: left;">
-    <a href="link per il logout" alt="aa">logout</a>
+    <a href="logout.php" alt="aa">logout</a>
     </td>
 </tr>
 </table>
