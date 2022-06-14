caf687183f4e855dec3fdf121aa9260bd0cb15d3
luigirispoli <102999484+luigirispoli@users.noreply.github.com>
2022-06-14 12:00:32 +0200

    Modifica utente

--- connessione.php
+++ connessione.php
@@ -5,6 +5,6 @@ $LWticket_table_name = "LWticket";
 $LWabbonamenti_table_name = "LWabbonamenti";
 $LWstreaming_table_name = "LWstreaming";
 
-$mysqliConnection = new mysqli("localhost", "lweb36", "lweb36", $db_name);
+$mysqliConnection = new mysqli("localhost", "lwebn", "lwebn", $db_name);
 
 ?>
